e046df75a02c0c6831d535971ec6480e48b36acb
Leonardo Feitosa <leonardofeitosa3010@gmail.com>
2020-06-13 19:58:52 -0300

    Implementando navegação inferior

--- lib/Home.dart
+++ lib/Home.dart
@@ -1,3 +1,7 @@
+import 'package:cloneyoutubeflutter/screens/Inicio.dart';
+import 'package:cloneyoutubeflutter/screens/EmAlta.dart';
+import 'package:cloneyoutubeflutter/screens/Inscricao.dart';
+import 'package:cloneyoutubeflutter/screens/Biblioteca.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
@@ -6,8 +10,19 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  int _indiceAtual = 0;
+
   @override
   Widget build(BuildContext context) {
+
+    List<Widget> telas = [
+      Inicio(),
+      EmAlta(),
+      Inscricao(),
+      Biblioteca(),
+    ];
+
     return Scaffold(
       appBar: AppBar(
         iconTheme: IconThemeData(
@@ -18,7 +33,6 @@ class _HomeState extends State<Home> {
           "assets/youtube.png",
           width: 98,
           height: 22,
-          //scale: 0.8,
         ),
         actions: <Widget>[
           IconButton(
@@ -35,7 +49,35 @@ class _HomeState extends State<Home> {
           )
         ],
       ),
-      body: Container(),
+      body: telas[_indiceAtual],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _indiceAtual,
+        onTap: (indice){
+          setState(() {
+            _indiceAtual = indice;
+          });
+        },
+        type: BottomNavigationBarType.fixed,
+        fixedColor: Colors.red,
+          items: [
+            BottomNavigationBarItem(
+              title: Text("Início"),
+              icon: Icon(Icons.home),
+            ),
+            BottomNavigationBarItem(
+              title: Text("Em alta"),
+              icon: Icon(Icons.whatshot),
+            ),
+            BottomNavigationBarItem(
+              title: Text("Inscrições"),
+              icon: Icon(Icons.subscriptions),
+            ),
+            BottomNavigationBarItem(
+              title: Text("Biblioteca"),
+              icon: Icon(Icons.folder),
+            ),
+          ],
+      ),
     );
   }
 }
